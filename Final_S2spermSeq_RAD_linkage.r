941d90df72af1d0dfe80f35649098abf751deb42
Smediterranea <huansci@gmail.com>
2020-08-12 10:04:44 -0700

    Update Final_S2spermSeq_RAD_linkage.r

--- Final_S2spermSeq_RAD_linkage.r
+++ Final_S2spermSeq_RAD_linkage.r
@@ -27,21 +27,6 @@ sg <- statGen(mapthis, bychr = FALSE, stat.type = "miss",id="CHROM_POS_REF_ALT")
 mapthis_miss <- subset(mapthis, ind = sg$miss < 50000)
 mapthis_miss <- drop.markers(mapthis_miss, todrop)
 mapthis_miss
-#genotyping clones; remove clones if needed
-gc <- genClones(mapthis_miss, tol = 0.95,id="locus_name")
-#segregation distortion
-profileMark(mapthis,stat.type = c("seg.dist","prop","miss"),crit.val = "bonf",layout=c(1,4),type="l",cex=0)
-profileMark(mapthis_miss,stat.type = c("seg.dist","prop","miss"),crit.val = "bonf",layout=c(1,3),type="l",cex=0)
-sg <- statMark(mapthis_miss, stat.type = "marker")
-mm <- statMark(mapthis_miss, stat.type = "marker")$marker$AA
-mapthis_seg <- drop.markers(mapthis_miss, c(markernames(mapthis_miss)[mm > 0.7],
-                                            markernames(mapthis_miss)[mm < 0.3]))
-mapthis_seg
-profileMark(mapthis_seg,stat.type = c("seg.dist","prop","miss"),crit.val = "bonf",layout=c(1,4),type="l",cex=0.4)
-rf <- pull.rf(mapthis_seg)
-lod <- pull.rf(mapthis_seg, what="lod")
-plot(as.numeric(rf), as.numeric(lod), xlab="Recombination fraction", ylab="LOD score")
-
 
 #####MSTmap variant filtering########
 mapBC <- pullCross(mapthis_miss, type = "missing", pars = list(miss.thresh =0.3))
@@ -52,10 +37,6 @@ sum(ncol(mapBC3$missing$data), ncol(mapBC3$seg.dist$data),
 mapBC
 mapBC2
 mapBC3
-profileMark(mapBC3,stat.type = c("seg.dist","prop","miss"),crit.val = "bonf",layout=c(1,4),type="l",cex=0.4)
-rf <- pull.rf(mapBC3)
-lod <- pull.rf(mapBC3, what="lod")
-plot(as.numeric(rf), as.numeric(lod), xlab="Recombination fraction", ylab="LOD score")
 
 #construct map
 mapBC4 <- mstmap(mapBC3, bychr = F, trace = TRUE, dist.fun =
@@ -69,7 +50,7 @@ toswitch <- markernames(mapBC4, chr=c("L.14","L.27","L.3","L.30","L.32","L.33","
 mapBC5 <- switchAlleles(mapBC4, toswitch)
 mapBC6 <- mstmap(mapBC5, bychr = F, trace = TRUE, dist.fun =
                    "kosambi", p.value = 1e-5,id="CHROM_POS_REF_ALT", objective.fun = "COUNT", mvest.bc = T, detectBadData = T, return.imputed = T)
-
+#interate
 toswitch <- markernames(mapBC6, chr=c("L.9","L.19","L.26","L.27","L.33","L.34","L.35","L.40","L.38","L.39"))
 mapBC7 <- switchAlleles(mapBC6, toswitch)
 mapBC8 <- mstmap(mapBC7, bychr = F, trace = TRUE, dist.fun =
@@ -77,23 +58,17 @@ mapBC8 <- mstmap(mapBC7, bychr = F, trace = TRUE, dist.fun =
 mapBC8 <- mstmap(mapBC8, bychr = T, trace = TRUE, dist.fun =
                    "kosambi", p.value = 2,id="CHROM_POS_REF_ALT", objective.fun = "COUNT", mvest.bc = T, detectBadData = T, return.imputed = T)
 
-#iterate
+#examine map
 heatMap(mapBC6, lmax = 6,color = rev(rainbow(256, start = 0, end = 2/3)))
 plotRF(mapBC6,alternate.chrid = T,zmax = 6,col.scheme = "redblue")
 plotRF(mapBC8,alternate.chrid = T,zmax = 6,col.scheme = "redblue")
-
-chrlen(mapBC6)
-
-rf <- pull.rf(mapBC8)
-lod <- pull.rf(mapBC8, what="lod")
-plot(as.numeric(rf), as.numeric(lod), xlab="Recombination fraction", ylab="LOD score")
-
+#save linkage
 marker_chr <- pull.map(mapBC8,as.table = T)
 write.table(marker_chr, file = "s2_linkage_rad_august_48sperms.txt",sep = "\t")
 
 #merge broken linkages into chromosomes
+#remove problematic linkage groups from chromosome assignment
 mapDHm <- mergeCross(mapBC8, merge = list(`chr1` = c("L.2", "L.21","L.24","L.33","L.34","L.36","L.4","L.38"), `chr2`= c("L.1", "L.15","L.12","L.13","L.14","L.16","L.17","L.18","L.20","L.25","L.26","L.28","L.27","L.5","L.6","L.7","L.8"),`chr3`=c("L.11","L.19","L.22","L.23","L.31","L.32","L.30","L.37","L.9"),`chr4`=c("L.10","L.35")))
-
 chrlen(mapDHm)
 plotRF(mapDHm,alternate.chrid = T,zmax = 5.5,col.scheme = "redblue")
 
@@ -102,7 +77,7 @@ mapBC14 <- mstmap(mapDHm, bychr = T, trace = TRUE, dist.fun = "kosambi", p.value
 chrlen(mapBC14)
 plotRF(mapBC14,alternate.chrid = T,zmax = 5.5,col.scheme = "redblue", cex=0.1,chr = c("chr1","chr2","chr3","chr4"))
 
+#save linkage and RDS
 marker_chr <- pull.map(mapBC14,as.table = T)
 write.table(marker_chr, file = "s2_linkage_rad_august_48sperms_merged.txt",sep = "\t")
-
 saveRDS(mapBC14, "s2_linkage_rad_august_48sperms_merged.rds")
